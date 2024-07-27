4e27e0a5876b0cf0ab1e5572837f0b2f60fcc1c2
Redian Marku <redian1marku@gmail.com>
2024-07-28 00:15:34 +0200

    Spring Core

--- 01-spring-boot-overview/03-actuator-demo/src/main/java/com/topnotchprogrammer/tnpdemo/rest/FunRESTController.java
+++ 01-spring-boot-overview/03-actuator-demo/src/main/java/com/topnotchprogrammer/tnpdemo/rest/FunRESTController.java
@@ -1,5 +1,6 @@
 package com.topnotchprogrammer.tnpdemo.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.batch.BatchTransactionManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,14 @@ public class FunRESTController {
     @GetMapping("/")
     public String sayHello(){
         return "Hello World";
+    }
+
+    @Value("${tnp.owner}")
+    private String owner;
+
+    @GetMapping("/owner")
+    public String showOwner(){
+        return owner;
     }
 
 }
